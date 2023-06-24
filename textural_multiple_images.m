204905b23fcd3bb9c299d03ddc4712f373da11ec
Gasby90 <59999806+Gasby90@users.noreply.github.com>
2023-06-24 16:18:45 +0200

    Update textural_multiple_images.m

--- textural_multiple_images.m
+++ textural_multiple_images.m
@@ -1,5 +1,18 @@
-%% TEXTURE ANALYSIS TOOLBOX light  
-% Riccardo Gasbarrone, 2017. Ver 1.0 (15 March 2017).
+%% textural_multiple_images.m  
+
+%%Copyright (C) 2023  Riccardo Gasbarrone
+
+%This program is free software; you can redistribute it and/or
+%modify it under the terms of the GNU General Public License
+%as published by the Free Software Foundation; either version 2
+%of the License, or  any later version.
+
+%This program is distributed in the hope that it will be useful,
+%but WITHOUT ANY WARRANTY; without even the implied warranty of
+%MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%GNU General Public License for more details.
+%%
+
 %This script allow to extract the gray-level co-occurrence matrices (at
 %=0°, 45°, 90° and 135°) of all images occurring in a folder, then the
 %Haralick textural features are computed for all the 4 direction. 
